cfda637e5bef92a85ba18eb85844a9e9f4ef007f
DavidMillan45 <89211677+DavidMillan45@users.noreply.github.com>
2023-06-08 13:37:11 -0500

    Add files via upload

---
+++ ACelular/ButtonHandler.java
@@ -0,0 +1,27 @@
+import java.awt.event.*;
+import javax.swing.JCheckBox;
+
+public class ButtonHandler implements ActionListener {
+
+	private JCheckBox box;
+	private Automata celular;
+	private Cuadro cuadroe;
+	
+	public ButtonHandler(Cuadro cuadro){
+		box = cuadro.getCheckBox();
+		cuadroe = cuadro;
+		celular = new Automata();
+	}
+	
+	public void actionPerformed(ActionEvent e){
+		celular.PATRON_RANDOM = (box.isSelected())?true:false;
+		celular.NUMERO_DE_REGLA = (e.getActionCommand() == "regla30")?30:0;
+		celular.NUMERO_DE_REGLA = (e.getActionCommand() == "regla90")?90:celular.NUMERO_DE_REGLA;
+		celular.NUMERO_DE_REGLA = (e.getActionCommand() == "regla110")?110:celular.NUMERO_DE_REGLA;
+		celular.NUMERO_DE_REGLA = (e.getActionCommand() == "regla150")?150:celular.NUMERO_DE_REGLA;
+		celular.NUMERO_DE_REGLA = (e.getActionCommand() == "regla184")?184:celular.NUMERO_DE_REGLA;
+		celular.mostrar();
+	}
+	
+
+}
